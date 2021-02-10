5fafcf696c0cf08f5341cd50a33a4ccb0e2c754c
LiubovKleimenova <kleimenovaliubov@gmail.com>
2021-02-09 19:43:55 -0500

    add delete route

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllTodos } = require("./APIs/todos");
+const { getAllTodos, postOneToDo, deleteToDo } = require("./APIs/todos");
+
+app.post("/todo", postOneToDo);
 
 app.get("/todos", getAllTodos);
+
+app.delete("/todo/:todoId", deleteToDo);
+
 exports.api = functions.https.onRequest(app);
